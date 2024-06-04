9e5dfd748d05f2ee4e8b83eab38186628729e7b4
julianildautarmo <149820753+julianildautarmo@users.noreply.github.com>
2024-06-04 06:57:29 +0000

    Assignment #2

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,7 @@
-#
+from 
+
+import multiply_numbers
+
+result = devprocess.multiply_numbers(6, 6)
+
+print(result)
--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,2 +1,5 @@
 def get_number(num):
     return num
+
+def multiply_numbers(num1,num2):   #product of 2 numbers
+    return num1*num2
