5ec7bc8d3bd0c04e3717bfbdaf296c03c930c529
julianildautarmo <149820753+julianildautarmo@users.noreply.github.com>
2024-07-09 23:03:34 +0000

    a

--- src/examples/f_files_exception/exceptions.py
+++ src/examples/f_files_exception/exceptions.py
@@ -1,2 +1,43 @@
 def test_config():
     return True
+
+def divide_a_number(num1,num2):
+
+    result = 0
+
+    if num2 != 0:
+        num1 / num2 == result
+
+    else:
+        return "\nDivision by 0 not allowed!\n"
+
+    return result
+
+
+def multiple_2_numbers():
+    
+    num1 = input(" Enter a number: ")
+    while num1.isalpha():
+        num1 = input("Enter digit(s): ")
+
+    num2 = input(" Enter a number: ")
+    while num2.isalpha():
+        num2 = input("Enter digit(s): ")
+
+    out = float(num1)*float(num2)#you can only float(number) not a letter
+    print(out)
+
+def open_fake_file():
+
+    try:
+        file = open("file_name", "r")
+
+        contents = file.read()
+
+        print(contents)
+
+    except IOError:
+        print("\nxxxxxxxxxxxxxxxxxx File cannot be found! xxxxxxxxxxxxxxxxxx\n")
+
+
+
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,6 +1,16 @@
 #main program
 import files
+import exceptions
 
-files.employee_filing('file.txt')
+#files.employee_filing('file.txt')
 
-files.read_records('file.txt')
+#files.read_records('file.txt')
+
+'''result = exceptions.divide_a_number(2,0)
+
+print(result)'''
+
+
+#exceptions.multiple_2_numbers()
+
+#exceptions.open_fake_file()
